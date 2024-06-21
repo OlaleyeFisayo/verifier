49eb1ae0fb57fe183da268ddfcee82442b02fe46
Festus-Olaleye Oluwafisayomi Oluwaseunfunmi <festusolaleyef@gmail.com>
2024-06-21 13:40:51 +0100

    ip address restriction

--- controllers/requery.controller.js
+++ controllers/requery.controller.js
@@ -13,19 +13,19 @@ export const requery = async (req, res) => {
   const { date, stan, rrn, amount } = value;
   const { year, month, day, hours, minutes, seconds } =
     separateDateComponents(date);
-  const query = await db.query(
+  const [query] = await db.query(
     `SELECT * FROM tms.transaction_record WHERE YEAR(transaction_time)=${year} AND MONTH(transaction_time)=${month} AND DAY(transaction_time)=${day} AND HOUR(transaction_time)=${hours} AND MINUTE(transaction_time)=${minutes} AND SECOND(transaction_time)=${seconds} AND stan=${stan} AND response_amount=${amount} AND retrieval_reference_number=${rrn}`,
   );
   const result = {
-    stan: query[0][0].stan,
-    date: query[0][0].transaction_date,
-    rrn: query[0][0].retrieval_reference_number,
-    response_code: query[0][0].response_code,
-    masked_pan: query[0][0].masked_pan,
-    amount: query[0][0].response_amount,
-    terminal_id: query[0][0].terminal_id,
-    emv_data_request: query[0][0].emv_data_request,
-    from_account_identification: query[0][0].from_account_identification,
+    stan: query[0].stan,
+    date: query[0].transaction_date,
+    rrn: query[0].retrieval_reference_number,
+    response_code: query[0].response_code,
+    masked_pan: query[0].masked_pan,
+    amount: query[0].response_amount,
+    terminal_id: query[0].terminal_id,
+    emv_data_request: query[0].emv_data_request,
+    from_account_identification: query[0].from_account_identification,
   };
   res.send(result);
 };
--- middleware/ipFilter.js
+++ middleware/ipFilter.js
@@ -1,11 +1,19 @@
 import CustomAPIError from "../errors/custom-error.js";
+import ipaddr from "ipaddr.js";
 
 const allowedIPs = ["127.0.0.1", "0.0.0.0"];
 
 export const ipFilter = (req, res, next) => {
   let clientIP = req.ip || req.connection.remoteAddress;
-  if (clientIP === "::1") {
-    clientIP = '127.0.0.1';
+  if (ipaddr.isValid(clientIP)) {
+    const parsedAddress = ipaddr.parse(clientIP);
+    if (parsedAddress.kind() === "ipv6") {
+      if (clientIP === "::1") {
+        clientIP = "127.0.0.1";
+      } else if (parsedAddress.isIPv4MappedAddress()) {
+        clientIP = parsedAddress.toIPv4Address().toString();
+      }
+    }
   }
   if (allowedIPs.includes(clientIP)) {
     next();
